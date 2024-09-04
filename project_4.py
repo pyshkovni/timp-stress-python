46b93af4e674d6d7e054cc11647f8c66014358b0
pyshkovni <guess.the.tutor@gmail.com>
2024-09-04 09:12:10 +0300

    Подготовил кейс

---
+++ project_4.py
@@ -0,0 +1,16 @@
+## project_4
+# телеграм-бот, который показывает информацию о погоде c Яндекса
+
+## Необходимые зависимости 
+# pyTelegramBotAPI
+# requests
+# могут потребоваться еще зависимости
+
+# Ссылка на бот: https://t.me/case1_project4_bot
+# Токен для бота можно взять по ссылке: https://disk.yandex.ru/d/7L5ZLkqZNQkdlg
+
+# Ссылка на документацию по Яндекс погоде
+# От туда лучше взять пример для доступа к погоде
+# https://yandex.ru/pogoda/b2b/console/api-page
+
+# Ключ для вставки в URL возьмите по ссылке: https://disk.yandex.ru/d/ZZXufLBA6G-0jQ
---
+++ project_6.py
@@ -0,0 +1,17 @@
+## project_6
+# Discord-bot, который показывает информацию о погоде c Яндекса
+
+## Необходимые зависимости 
+# discord.py
+# random
+# могут потребоваться еще зависимости
+
+# Приглашение на сервер discord с ботом: https://discord.gg/YZQCg6KW
+
+# Токен для бота можно взять по ссылке: https://disk.yandex.ru/d/wmxyxzKJHSmQtw
+
+# Ссылка на документацию по Яндекс погоде
+# От туда лучше взять пример для доступа к погоде
+# https://yandex.ru/pogoda/b2b/console/api-page
+
+# Ключ для вставки в URL возьмите по ссылке: https://disk.yandex.ru/d/ZZXufLBA6G-0jQ
