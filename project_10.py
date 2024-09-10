e624a0dcd96861e96efccd355837ff495b32fdbc
pyshkovni <guess.the.tutor@gmail.com>
2024-09-10 19:14:21 +0300

    Дополнил проекты

--- project_10.py
+++ project_10.py
@@ -1,1 +1,10 @@
-# тг-бот переводчик https://yandex.cloud/ru/docs/translate/operations/translate#python_1
+## project_3
+# Телеграм-бот, который переводит передаваемые ему сообщения с помощью Yandex Translate
+
+## Необходимые зависимости 
+# pyTelegramBotAPI
+# requests
+# могут потребоваться еще зависимости
+
+# Ссылка на бот: https://t.me/case1_project3_bot
+# Токен для бота можно взять по ссылке: https://disk.yandex.ru/d/SZUbCUanyi9Ptg
