90216209b3a50d4600b82108ecc7656fc0d9f83b
pyshkovni <guess.the.tutor@gmail.com>
2024-09-10 18:44:19 +0300

    описал проекты

--- project_6.py
+++ project_6.py
@@ -6,6 +6,8 @@
 # requests
 # могут потребоваться еще зависимости
 
+# Приглашение на сервер discord с ботом: https://discord.gg/YZQCg6KW
+# Токен для бота можно взять по ссылке: https://disk.yandex.ru/d/wmxyxzKJHSmQtw
 
 # Ссылка на источник
 # https://open-meteo.com/
